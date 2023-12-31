661b3a5d44fac835ccc378c777abbc624067dbea
YurkPS <yurkovps@yandex.ru>
2024-01-01 01:58:17 +0300

    Введение элементов исходного массива и вывод их на экран

--- DZ_Attest/Program.cs
+++ DZ_Attest/Program.cs
@@ -1,3 +1,24 @@
-﻿Console.WriteLine("Введите количество элементов массива:");
-int arraySize = Convert.ToInt32(Console.ReadLine());
-string[] Array = new string[arraySize];
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество элементов массива:");
+        int arraySize = Convert.ToInt32(Console.ReadLine());
+        string[] Array = new string[arraySize];
+        
+        Console.WriteLine($"Введите {arraySize} элемента(ов) массива, используя ввод:");
+        for (int i = 0; i < arraySize; i++)
+        {
+            Array[i] = Console.ReadLine();
+        }
+
+        Console.Clear();
+        Console.WriteLine("Первоначальный массив:");
+        foreach (var str in Array)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
